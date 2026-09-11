a38960692eb8571e0b4d1b59f1e346140043abc9
MaxChip101 <1max0lag1@gmail.com>
2026-09-11 09:45:49 -0600

    fixed the broken scripts:w

--- init.lua
+++ init.lua
@@ -2,24 +2,30 @@ require("maxchip.settings")
 require("maxchip.keybinds")
 require("maxchip.lazy")
 
-local lspconfig = require('lspconfig')
-
--- C/C++
-lspconfig.clangd.setup({})
-
--- Zig
-lspconfig.zls.setup({})
 
 -- Lua
-lspconfig.lua_ls.setup({
-    settings = {
-	Lua = {
-	    diagnostics = {
-		globals = { 'vim' },
-	    },
-	    workspace = {
-		library = vim.api.nvim_get_runtime_file("", true),
-	    },
-	},
-    },
+--lspconfig.lua_ls.setup({
+--    settings = {
+--	Lua = {
+--	    diagnostics = {
+--		globals = { 'vim' },
+--	    },
+--	    workspace = {
+--		library = vim.api.nvim_get_runtime_file("", true),
+--	    },
+--	},
+--    },
+--})
+
+vim.lsp.config('*', {
+    root_markers = { '.git' },
+})
+
+vim.lsp.enable({
+    'gopls',
+    'pyright',
+    'zls',
+    'rust_analyzer',
+    'nixd',
+    'lua_ls',
 })
--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,12 +1,13 @@
 return {
     'nvim-treesitter/nvim-treesitter',
     build = ':TSUpdate',
+    branch = 'master',
     config = function()
 	local configs = require("nvim-treesitter.configs")
 	configs.setup ({
 	    highlight = { enable = true },
 	    indent = { enable = true },
-	    autotage = { enable = true },
+	    autotag = { enable = true },
 	    ensure_installed = {
 		"lua",
 		"c",
