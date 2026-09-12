71eb17b1fb39607d287b33ee57ffcf1f076851a0
MaxChip101 <1max0lag1@gmail.com>
2026-09-11 20:10:39 -0600

    organized configurations.nix

--- configuration.nix
+++ configuration.nix
@@ -1,12 +1,29 @@
 environment.systemPackages = with pkgs; [
+    # neovim & lazy.vim & plugin dependencies
+    neovim
+    git
     tree-sitter
-    pkgs.clang-tools
+    gcc
+    gnumake
+    curl
+    ripgrep
+    fd
+
+    # keyboards
+    wl-clipboard
+    xclip
+
+    # lsps & fmts
+    clang-tools
     gopls
     pyright
     zls
     rust-analyzer
     nixd
     lua-language-server
-
-
+    stylua
+    black
+    go
+    rustfmt
+    zig_0_16
 ];
